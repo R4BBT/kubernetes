b72df2d6f8b3fa1fdb86bcc29ed187e0c5c96627
R4BBT <ryanfangk@gmail.com>
2021-04-08 20:09:09 +0000

    04/08/2021

--- install-kubeadm.sh
+++ install-kubeadm.sh
@@ -43,5 +43,4 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
-
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
