78255ea495e45769353063a4bdb8a6785e57a722
R4BBT <ryanfangk@gmail.com>
2021-04-08 20:18:23 +0000

    04/08/2021

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ echo ":set number paste" >> .vimrc
 echo ":set shiftwidth=4 tabstop=2 expandtab" >> .vimrc
 echo "alias k='kubectl'">> .bashrc
 
-sudo . ./.bashrc
+sudo source ./.bashrc
 
 echo "Your environment has been set up."
